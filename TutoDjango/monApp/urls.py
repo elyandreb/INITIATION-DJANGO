7af4b17cfd2180222aea55aacbb76350532c4b40
elyandreb <elyandreburet72@gmail.com>
2025-09-04 08:58:46 +0200

    petit challenge (page contact+apropos)

--- TutoDjango/monApp/urls.py
+++ TutoDjango/monApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('home/<param>',views.home ,name='home'),
+    path("contact", views.contact, name="contact"),
+    path("info", views.apropos, name="apropos"),
 ]
--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("<h1>Hello Django!</h1>")
+def home(request, param):
+    return HttpResponse("<h1>Bonjour "+param+" !</h1>")
+
+def contact(request):
+    return HttpResponse("<h1> Contactez nous</h1> <p> Ceci est un test </p>")
+
+def apropos(request):
+    return HttpResponse("<h1> About us</h1> <p> Ceci est un test </p>")
