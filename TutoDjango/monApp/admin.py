3331bdfb0a78900a901f519c601261c5e687dc6c
elyandreb <elyandreburet72@gmail.com>
2025-09-11 09:25:37 +0200

    Personnalisation interface admin

--- TutoDjango/monApp/admin.py
+++ TutoDjango/monApp/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from django.contrib import admin
-from .models import Produit, Categorie, Statut, Rayon
+from .models import Contenir, Produit, Categorie, Statut, Rayon
 
-admin.site.register(Produit)
-admin.site.register(Categorie)
+
+class ProduitAdmin(admin.ModelAdmin):
+    list_display = ('intituleProd', 'prixUnitaireProd')
+
+admin.site.register(Produit, ProduitAdmin)
+
+class ProduitInline(admin.TabularInline):
+    model = Produit
+    extra = 1  # nombre de lignes vides par défaut
+    
+class CategorieAdmin(admin.ModelAdmin):
+    model = Categorie
+    inlines = [ProduitInline]
+
+admin.site.register(Categorie, CategorieAdmin)
 admin.site.register(Statut)
 admin.site.register(Rayon)
 
