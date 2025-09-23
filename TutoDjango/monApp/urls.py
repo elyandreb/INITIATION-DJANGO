e2b834d1fd9146802eeebca6403831c2b2f271ee
elyandreb <elyandreburet72@gmail.com>
2025-09-23 18:21:41 +0200

    Fin La vue générique de base

--- TutoDjango/monApp/urls.py
+++ TutoDjango/monApp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
+from django.views.generic import *
 
 urlpatterns = [
-    path("home/<param>",views.accueil ,name='accueil'),
-    path("contact/", views.contact, name="contact"),
-    path("info/", views.apropos, name="apropos"),
+    #path("home/<param>",views.accueil ,name='accueil'),
+    path("contact/", views.ContactView.as_view(), name="contact"),
+    path("info/", views.AboutView.as_view(), name="apropos"),
     path("produits/", views.listeproduits, name="listeproduits"),
     path("categories/", views.listecat, name="listecat"),
     path("statuts/", views.listestatut, name="listestatut"),
     path("rayons/", views.listerayons, name="listerayons"),
+    path("home/", views.HomeView.as_view()),
+    path("home/<param>", views.HomeView.as_view())
 ]
--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from monApp.models import Categorie, Produit, Rayon, Statut
-
+from django.views.generic import *
 
 def accueil(request,param):
     return HttpResponse("<h1>Hello " + param + " ! You're connected</h1>")
@@ -28,3 +28,38 @@ def listestatut(request):
 def listerayons(request):
     rays = Rayon.objects.all()
     return render(request, 'monApp/list_rayon.html',{'rays': rays})
+
+
+class HomeView(TemplateView):
+    template_name = "monApp/page_home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['titreh1'] = "Hello Django " + self.kwargs.get('param', '')
+        return context
+    
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
+    
+
+class AboutView(TemplateView):
+    template_name = "monApp/page_home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        context['titreh1'] = "About us"
+        return context
+    
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
+    
+class ContactView(TemplateView):
+    template_name = "monApp/page_home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactView, self).get_context_data(**kwargs)
+        context['titreh1'] = "Contact us"
+        return context
+    
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
