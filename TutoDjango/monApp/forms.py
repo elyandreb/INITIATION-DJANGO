c74e0341b900398e89779a7e00eb63f574ad4a71
elyandreb <elyandreburet72@gmail.com>
2025-09-30 17:43:57 +0200

    resolution erreur

--- TutoDjango/monApp/forms.py
+++ TutoDjango/monApp/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 
+from monApp.models import Produit
+
 class ContactUsForm(forms.Form):
     name = forms.CharField(required=False)
     email = forms.EmailField()
     message = forms.CharField(max_length=1000)
+
+class ProduitForm(forms.ModelForm):
+    class Meta:
+        model = Produit
+        #fields = '__all__'
+        exclude = ('categorie', 'statut')
